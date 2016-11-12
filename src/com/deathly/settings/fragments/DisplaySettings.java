1dc116021a9972eb10b942e36471522e4906b394
beanstown106 <nbenis106@gmail.com>
2016-11-11 21:29:28 -0500

    Advanced screenshot functionality [2/2]
    Change-Id: I471d6caffd08683b163347bf52359039475b7d80

--- src/com/deathly/settings/fragments/DisplaySettings.java
+++ src/com/deathly/settings/fragments/DisplaySettings.java
@@ -16,8 +16,12 @@
 
 package com.deathly.settings.fragments;
 
+import android.content.ContentResolver;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.PreferenceScreen;
 
@@ -26,12 +30,16 @@ import com.android.internal.logging.MetricsProto.MetricsEvent;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
-public class DisplaySettings extends SettingsPreferenceFragment {
+public class DisplaySettings extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
 
     private static final String KEY_NOTIFICATION_LIGHT = "notification_light";
     private static final String KEY_BATTERY_LIGHT = "battery_light";
+    private static final String SCREENRECORD_CHORD_TYPE = "screenrecord_chord_type";
 
     private static final String CATEGORY_LEDS = "leds";
+
+    private ListPreference mScreenrecordChordType;
 
     private Preference mNotifLedFrag;
     private Preference mBattLedFrag;
@@ -45,7 +53,8 @@ public class DisplaySettings extends SettingsPreferenceFragment {
 
         final PreferenceCategory leds = (PreferenceCategory) findPreference(CATEGORY_LEDS);
 
-/**        mNotifLedFrag = findPreference(KEY_NOTIFICATION_LIGHT);
+/*
+        mNotifLedFrag = findPreference(KEY_NOTIFICATION_LIGHT);
         //remove notification led settings if device doesnt support it
         if (!getResources().getBoolean(
                 com.android.internal.R.bool.config_intrusiveNotificationLed)) {
@@ -64,12 +73,41 @@ public class DisplaySettings extends SettingsPreferenceFragment {
                 com.android.internal.R.bool.config_intrusiveNotificationLed)
                 && !getResources().getBoolean(
                 com.android.internal.R.bool.config_intrusiveBatteryLed)) {
-            prefScreen.removePreference(findPreference(CATEGORY_LEDS)); **/
-        }
+            prefScreen.removePreference(findPreference(CATEGORY_LEDS)); */ 
+        int recordChordValue = Settings.System.getInt(resolver,
+                Settings.System.SCREENRECORD_CHORD_TYPE, 0);
+        mScreenrecordChordType = initActionList(SCREENRECORD_CHORD_TYPE,
+                recordChordValue);
     }
 
     @Override
     protected int getMetricsCategory() {
         return MetricsEvent.DEATHLY;
     }
+
+    private ListPreference initActionList(String key, int value) {
+	        ListPreference list = (ListPreference) getPreferenceScreen().findPreference(key);
+	        list.setValue(Integer.toString(value));
+	        list.setSummary(list.getEntry());
+	        list.setOnPreferenceChangeListener(this);
+	        return list;
+	}
+	
+    private void handleActionListChange(ListPreference pref, Object newValue, String setting) {
+        String value = (String) newValue;
+        int index = pref.findIndexOfValue(value);
+        pref.setSummary(pref.getEntries()[index]);
+        Settings.System.putInt(getActivity().getContentResolver(), setting, Integer.valueOf(value));
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if  (preference == mScreenrecordChordType) {
+            handleActionListChange(mScreenrecordChordType, newValue,
+                    Settings.System.SCREENRECORD_CHORD_TYPE);
+            return true;
+        }
+        return false;
+    }
 }
+
